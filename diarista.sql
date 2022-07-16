bf21cd153b1d3e8230a190b88698a405302ef7ca
Fablena Kathllen <fablena_dias@hotmail.com>
2022-07-16 14:26:42 -0300

    adicionando os dados das diaristas

---
+++ diarista.sql
@@ -0,0 +1,12 @@
+SELECT * FROM db_faxinas.diarista;
+
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("08635224556", "Carmen Silva");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("05569742502", "Roberta Nascimento");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("01258963540", "Maria de Souza");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("12369874402", "Fernanda Soares");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("85236412501", "Maria Lurdes Costa");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("85423652200", "Nazaré Silva");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("85560085104", "Graça Lima");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("01288720035", "Manoel Pereira");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("04055530008", "Maria de Nazaré");
+INSERT INTO diarista(cpf_diarista, nome_diarista) VALUES ("74752448010", "Marinalva Souza");
