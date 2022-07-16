ba38601239a0c3296ee11e4e2ce676827fe46689
Fablena Kathllen <fablena_dias@hotmail.com>
2022-07-16 14:41:38 -0300

    adicionando os dados dos proprietarios

---
+++ proprietario.sql
@@ -0,0 +1,11 @@
+SELECT * FROM db_faxinas.proprietario;
+
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("01785223656", "Jade Oliveira");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("06658923111", "Elizabeth Soares");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("05884452545", "Cleiton Gonçalves");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("01101256301", "Ricardo Paiva");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("01775532615", "Jessica Meirelles");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("06584584110", "Ana Carolina Santos");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("48522114332", "Jonathan Dias");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("04228665442", "Helena Martins");
+INSERT INTO proprietario(cpf_proprietario, nome_proprietario) VALUES ("74785442701", "Felipe Daniel");
